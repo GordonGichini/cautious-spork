f0494fb52268715f7cba4f12635debd3f90f8551
GordonGichini <gordongichini1@gmail.com>
2025-01-18 13:54:02 +0300

    paddleleft up and down movement

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -16,16 +16,36 @@ class Game extends Phaser.Scene
 
         ball.body.setCollideWorldBounds(true, 1, 1)
 
-        ball.body.setVelocity(-200, 0)
+        ball.body.setVelocity(-200, 100)
 
-        const paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff)
-        this.physics.add.existing(paddleLeft, true)
+        this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff)
+        this.physics.add.existing(this.paddleLeft, true)
       //  paddleLeft.body.setBounce(1, 1)
         
         /** @type (Phaser.Physics.Arcade.Body) */
       //  const body = paddleLeft.body
 
-        this.physics.add.collider(paddleLeft, ball)
+        this.physics.add.collider(this.paddleLeft, ball)
+
+        this.cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update()
+    {
+        /** @type (Phaser.Physics.Arcade.StaticBody) */
+        const body = this.paddleLeft.body
+
+        if (this.cursors.up.isDown)
+        {
+            this.paddleLeft.y -= 10
+            body.updateFromGameObject()
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.paddleLeft.y += 10
+            body.updateFromGameObject()
+        }
+
     }
 }
 
